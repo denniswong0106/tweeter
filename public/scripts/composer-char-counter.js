2c312f2b26d095ecb7699ebf457cf7fcf3ac6d1b
Dennis Wong <denniswong0106@gmail.com>
2021-03-24 02:08:53 +0000

    add new js function wordCount (in composer-char-counter.js) that allows for display of wordcount on my webapp

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  
+  $("textarea").on('input', wordCount);
+});
+
+const wordCount = function() {
+
+  let remainingLettersObj = $(this).siblings().find("output");
+  let wordsInTextBox = $(this).val().length;
+  remainingLettersObj.val(140 - wordsInTextBox);
+
+  if (remainingLettersObj.val() < 0) {
+    remainingLettersObj.addClass('negative');
+  }
+
+  if (remainingLettersObj.val() >= 0) {
+    remainingLettersObj.removeClass('negative');
+  }
+}
