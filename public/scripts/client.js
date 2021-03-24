68c30b66cbc32e297645d20ba56d55bc46c1b809
Dennis Wong <denniswong0106@gmail.com>
2021-03-24 21:41:00 +0000

    add user avatar to each tweet by changing tweet.css to display correctly. client.js is updated to show display the avatars on each tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
         "handle": "@SirIsaac"
       },
       "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giantsasdfffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+        "text": "If I have seen further it is by standing on the shoulders of giantsasdfffffffffffffff"
       },
       "created_at": 1461116232227
     },
@@ -37,7 +37,7 @@ $(document).ready(function() {
 
   const createTweetElement = function (tweetObj) {
     console.log(tweetObj)
-    const tweetHeader = `<header><span>${tweetObj.user.name}</span><span id="handle" >${tweetObj.user.handle}</span></header>`;
+    const tweetHeader = `<header><span><span class="user-avatar"><img src=${tweetObj.user.avatars}></span>${tweetObj.user.name}</span><span id="handle" >${tweetObj.user.handle}</span></header>`;
     const tweetContent = `<div>${tweetObj.content.text}</div>`;
     const tweetFooter = `<footer> 
     <span>${tweetObj.created_at}</span>
