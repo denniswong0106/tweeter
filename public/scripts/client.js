788f03a148af617e7e44691a103e66db858c47fb
Dennis Wong <denniswong0106@gmail.com>
2021-03-25 05:06:47 +0000

    moved my function expressions outside document ready for readability.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,6 @@
+// --------------------------------------//
 
-
-$(document).ready(function() {
+// Functions //
 
   // Allows us to 'escape' potentially dangerous xss attacks; ie html code given as 
   // user inputs
@@ -43,44 +43,55 @@ $(document).ready(function() {
     return value;
   }
 
+  // clear the textarea text
+
+  const clearTextArea = function(value) {
+    $('textarea').val('');
+    return value;
+  }
+
   // loadtweets makes a GET request to receieve an array of tweets as JSON.
   const loadtweets = function() {
     $.ajax({method: 'get', url: '/tweets/'})
     .then(renderTweets)
     .catch()
   }
+
+
+$(document).ready(function() {
+  // Loads in database tweets once document is ready.
+  
   loadtweets();
   
   // initiates our actions when the "TWEET" button is pressed; we check for valid input
   // and if the input is valid, we store in database, then clear our tweets on page, 
   // then reload page with tweets udpated by database
-  
+
   $('form').on('submit', function(event) {
+    
     event.preventDefault();
-    $('.error-message').slideUp(300);
 
-    let $tweet = $(this).children('textarea').val();
+    $('.error-message').slideUp(300, () => {
+      let $tweet = $(this).children('textarea').val();
 
-    if ($tweet.length > 140) {
-      $('.error-message').text('You put too many Letters!')
-      $('.error-message').slideDown( 600);
-      return;
-    }
-    if (!$tweet) {
-      $('.error-message').text('HELLO?! Put some words in')
-      $('.error-message').slideDown( 600);
-      return;
-    }
-    if ($tweet) {
-      $.ajax({method: 'post', url: '/tweets/', data: $(this).serialize() })
-      .then(clearTweets)
-      .then(loadtweets);
-      return;
-    }
-
+      if ($tweet.length > 140) {
+        $('.error-message').text('You put too many Letters!')
+        $('.error-message').slideDown(600);
+        return;
+      }
+      if (!$tweet) {
+        $('.error-message').text('HELLO?! Put some words in')
+        $('.error-message').slideDown(600);
+        return;
+      }
+      if ($tweet) {
+        $.ajax({method: 'post', url: '/tweets/', data: $(this).serialize() })
+        .then(clearTweets)
+        .then(clearTextArea)
+        .then(loadtweets);
+        return;
+      }
+    })
   }); 
-
-
-  
 
 })
