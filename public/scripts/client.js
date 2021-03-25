e74df94f8fa202eccd56c5851de8282aa9551b9f
Dennis Wong <denniswong0106@gmail.com>
2021-03-25 02:10:58 +0000

    add comments in client.js to make clearer the purpose of each function and each function call

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,49 +1,59 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+
 
 $(document).ready(function() {
 
+  // Allows us to 'escape' potentially dangerous xss attacks; ie html code given as 
+  // user inputs
   const escape = function(str) {
     let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
-  }
+  };
 
   // createTweetElement function - takes in tweet object, returns tweet article
   // containing entire html structure of the tweet
-
   const createTweetElement = function (tweetObj) {
 
     const tweetHeader = `<header><span><span class="user-avatar"><img src=${tweetObj.user.avatars}></span>${tweetObj.user.name}</span><span id="handle" >${tweetObj.user.handle}</span></header>`;
-    const tweetContentSafe = `<div>${escape(tweetObj.content.text)}</div>`;
-    const tweetFooter = `<footer> 
-    <span>${tweetObj.created_at}</span>
-    <span> 
-      <i class="fas fa-flag"></i>
-      <i class="fas fa-retweet"></i>
-      <i class="fas fa-heart"></i>
-    </span>
-    </footer>`
-    return $(`<article class="tweet">${tweetHeader}${tweetContentSafe}${tweetFooter}</article>`);
+    const tweetContent = `<div>${escape(tweetObj.content.text)}</div>`;
+    const tweetFooter = `<footer>
+      <span>${tweetObj.created_at}</span>
+      <span>
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </span>
+      </footer>`;
 
-  }
+    return $(`<article class="tweet">${tweetHeader}${tweetContent}${tweetFooter}</article>`);
+  };
 
-
-
+  // takes an array of Tweet objects and uses each tweet object information to render
+  // a tweet on the page 
   const renderTweets = function(arrOfTweetObjs) {
-    arrOfTweetObjs.forEach(tweetObj => {
-      $('#tweets-container').append(createTweetElement(tweetObj));
 
-    });
-  }
+    arrOfTweetObjs.forEach(tweetObj => {
+      $('#tweets-container').prepend(createTweetElement(tweetObj));
+    })
+  };
+
+  // clear tweets clears all the stored DOM tweets on the html page
   const clearTweets = function(value) {
     $('.tweet').remove();
     return value;
   }
 
+  // loadtweets makes a GET request to receieve an array of tweets as JSON.
+  const loadtweets = function() {
+    $.ajax({method: 'get', url: '/tweets/'})
+    .then(renderTweets)
+    .catch()
+  }
+  loadtweets();
+  
+  // initiates our actions when the "TWEET" button is pressed; we check for valid input
+  // and if the input is valid, we store in database, then clear our tweets on page, 
+  // then reload page with tweets udpated by database
   $('form').on('submit', function(event) {
     event.preventDefault();
 
@@ -66,14 +76,6 @@ $(document).ready(function() {
 
   }); 
 
-  // loadtweets makes a GET request to receieve an array of tweets as JSON.
-
-  const loadtweets = function() {
-    $.ajax({method: 'get', url: '/tweets/'})
-    .then(renderTweets)
-    .catch()
-  }
-  loadtweets();
 
   
 
