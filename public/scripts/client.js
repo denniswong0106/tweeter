a461558510c886068a71ae43f14991016be91683
Dennis Wong <denniswong0106@gmail.com>
2021-03-25 05:10:17 +0000

    small formatting code fixes

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,61 +2,62 @@
 
 // Functions //
 
-  // Allows us to 'escape' potentially dangerous xss attacks; ie html code given as 
-  // user inputs
-  const escape = function(str) {
-    let div = document.createElement('div');
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  };
+// Allows us to 'escape' potentially dangerous xss attacks; ie html code given as 
+// user inputs
+const escape = function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 
-  // createTweetElement function - takes in tweet object, returns tweet article
-  // containing entire html structure of the tweet
-  const createTweetElement = function (tweetObj) {
+// createTweetElement function - takes in tweet object, returns tweet article
+// containing entire html structure of the tweet
+const createTweetElement = function (tweetObj) {
 
-    const tweetHeader = `<header><span><span class="user-avatar"><img src=${tweetObj.user.avatars}></span>${tweetObj.user.name}</span><span id="handle" >${tweetObj.user.handle}</span></header>`;
-    const tweetContent = `<div>${escape(tweetObj.content.text)}</div>`;
-    const tweetFooter = `<footer>
-      <span>${tweetObj.created_at}</span>
-      <span>
-        <i class="fas fa-flag"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="fas fa-heart"></i>
-      </span>
-      </footer>`;
+  const tweetHeader = `<header><span><span class="user-avatar"><img src=${tweetObj.user.avatars}></span>${tweetObj.user.name}</span><span id="handle" >${tweetObj.user.handle}</span></header>`;
+  const tweetContent = `<div>${escape(tweetObj.content.text)}</div>`;
+  const tweetFooter = `<footer>
+    <span>${tweetObj.created_at}</span>
+    <span>
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-heart"></i>
+    </span>
+    </footer>`;
 
-    return $(`<article class="tweet">${tweetHeader}${tweetContent}${tweetFooter}</article>`);
-  };
+  return $(`<article class="tweet">${tweetHeader}${tweetContent}${tweetFooter}</article>`);
+};
 
-  // takes an array of Tweet objects and uses each tweet object information to render
-  // a tweet on the page 
-  const renderTweets = function(arrOfTweetObjs) {
+// takes an array of Tweet objects and uses each tweet object information to render
+// a tweet on the page 
+const renderTweets = function(arrOfTweetObjs) {
 
-    arrOfTweetObjs.forEach(tweetObj => {
-      $('#tweets-container').prepend(createTweetElement(tweetObj));
-    })
-  };
+  arrOfTweetObjs.forEach(tweetObj => {
+    $('#tweets-container').prepend(createTweetElement(tweetObj));
+  })
+};
 
-  // clear tweets clears all the stored DOM tweets on the html page
-  const clearTweets = function(value) {
-    $('.tweet').remove();
-    return value;
-  }
+// clear tweets clears all the stored DOM tweets on the html page
+const clearTweets = function(value) {
+  $('.tweet').remove();
+  return value;
+}
 
-  // clear the textarea text
+// clear the textarea text
 
-  const clearTextArea = function(value) {
-    $('textarea').val('');
-    return value;
-  }
+const clearTextArea = function(value) {
+  $('textarea').val('');
+  return value;
+}
 
-  // loadtweets makes a GET request to receieve an array of tweets as JSON.
-  const loadtweets = function() {
-    $.ajax({method: 'get', url: '/tweets/'})
-    .then(renderTweets)
-    .catch()
-  }
+// loadtweets makes a GET request to receieve an array of tweets as JSON.
+const loadtweets = function() {
+  $.ajax({method: 'get', url: '/tweets/'})
+  .then(renderTweets)
+  .catch()
+}
 
+// --------------------------------------------------------------//
 
 $(document).ready(function() {
   // Loads in database tweets once document is ready.
@@ -79,11 +80,13 @@ $(document).ready(function() {
         $('.error-message').slideDown(600);
         return;
       }
+
       if (!$tweet) {
         $('.error-message').text('HELLO?! Put some words in')
         $('.error-message').slideDown(600);
         return;
       }
+
       if ($tweet) {
         $.ajax({method: 'post', url: '/tweets/', data: $(this).serialize() })
         .then(clearTweets)
@@ -91,7 +94,7 @@ $(document).ready(function() {
         .then(loadtweets);
         return;
       }
-    })
-  }); 
 
-})
+    })
+  })
+});
