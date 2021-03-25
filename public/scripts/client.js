66d7d6e169e3f597b832310d1c7eed517096e6dc
Dennis Wong <denniswong0106@gmail.com>
2021-03-25 02:00:18 +0000

    in this branch, I will leverage jquerys to display more user friendly error messages for invalid entrys (as opposed to using alerts)

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,7 +6,7 @@
 
 $(document).ready(function() {
 
-  const escape =  function(str) {
+  const escape = function(str) {
     let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
