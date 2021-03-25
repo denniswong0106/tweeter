6dc48a31e94aa14aad0e934f2e3dfa5a3f72b3bb
Dennis Wong <denniswong0106@gmail.com>
2021-03-25 21:37:45 +0000

    add functionality of back to top button. Button shows up when page is scrolled down a certain amount, and disappears when scrolled back up (needs a bit of calibration). Button not functional yet (cannot be clicked)

--- public/scripts/toggle-new-tweet.js
+++ public/scripts/toggle-new-tweet.js
@@ -1,10 +1,22 @@
 $(document).ready(function() {
   
   $('.toggle-new-tweet').on('click', toggleNewTweet);
-
+  $(window).on('scroll', toggleScrollTop);
 });
 
 const toggleNewTweet = () => {
   $(".new-tweet").toggle(400);
   $('textarea').focus();
+}
+
+const toggleScrollTop = function() {
+  console.log('scrolltop:', $(this).scrollTop())
+  if ($(this).scrollTop() > 300) {
+    $('.scroll-top').toggle(true);
+    return;
+  }
+  if ($(this).scrollTop() < 300) {
+    $('.scroll-top').toggle(false);
+  }
+
 }
