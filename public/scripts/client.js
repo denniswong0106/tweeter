aae4781344ef64e2d4914f9fe47353347fd95b61
Dennis Wong <denniswong0106@gmail.com>
2021-03-26 02:36:55 +0000

    installed momentjs to display time in 'x days/hours/minutes ago' format, put link into the head of index.html. Updated client.js createTweetElement function to use momemnt. Currently time displays as intended

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,63 +1,4 @@
-// --------------------------------------//
-
-// Functions //
-
-// Allows us to 'escape' potentially dangerous xss attacks; ie html code given as 
-// user inputs
-const escape = function(str) {
-  let div = document.createElement('div');
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-};
-
-// createTweetElement function - takes in tweet object, returns tweet article
-// containing entire html structure of the tweet
-const createTweetElement = function (tweetObj) {
-
-  const tweetHeader = `<header><span><span class="user-avatar"><img src=${tweetObj.user.avatars}></span>${tweetObj.user.name}</span><span id="handle" >${tweetObj.user.handle}</span></header>`;
-  const tweetContent = `<div>${escape(tweetObj.content.text)}</div>`;
-  const tweetFooter = `<footer>
-    <span>${tweetObj.created_at}</span>
-    <span class=icons>
-      <i class="fas fa-flag"></i>
-      <i class="fas fa-retweet"></i>
-      <i class="fas fa-heart"></i>
-    </span>
-    </footer>`;
-
-  return $(`<article class="tweet">${tweetHeader}${tweetContent}${tweetFooter}</article>`);
-};
-
-// takes an array of Tweet objects and uses each tweet object information to render
-// a tweet on the page 
-const renderTweets = function(arrOfTweetObjs) {
-
-  arrOfTweetObjs.forEach(tweetObj => {
-    $('#tweets-container').prepend(createTweetElement(tweetObj));
-  })
-};
-
-// clear tweets clears all the stored DOM tweets on the html page
-const clearTweets = function(value) {
-  $('.tweet').remove();
-  return value;
-}
-
-// clear the textarea text
-
-const clearTextArea = function(value) {
-  $('textarea').val('');
-  $('.counter').val(140);
-  return value;
-}
-
-// loadtweets makes a GET request to receieve an array of tweets as JSON.
-const loadtweets = function() {
-  $.ajax({method: 'get', url: '/tweets/'})
-  .then(renderTweets)
-  .catch()
-}
-
+// const moment = require('moment');
 // --------------------------------------------------------------//
 
 $(document).ready(function() {
@@ -99,3 +40,67 @@ $(document).ready(function() {
     })
   })
 });
+
+// --------------------------------------//
+
+// Functions //
+
+// Allows us to 'escape' potentially dangerous xss attacks; ie html code given as 
+// user inputs
+const escape = function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
+// createTweetElement function - takes in tweet object, returns tweet article
+// containing entire html structure of the tweet
+const createTweetElement = function (tweetObj) {
+
+  const tweetHeader = `<header><span><span class="user-avatar"><img src=${tweetObj.user.avatars}></span>${tweetObj.user.name}</span><span id="handle" >${tweetObj.user.handle}</span></header>`;
+  const tweetContent = `<div>${escape(tweetObj.content.text)}</div>`;
+  const tweetFooter = `<footer>
+    <span>${moment(tweetObj.created_at).fromNow()}</span>
+    <span class=icons>
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-heart"></i>
+    </span>
+    </footer>`;
+
+  return $(`<article class="tweet">${tweetHeader}${tweetContent}${tweetFooter}</article>`);
+};
+
+// takes an array of Tweet objects and uses each tweet object information to render
+// a tweet on the page 
+const renderTweets = function(arrOfTweetObjs) {
+
+  arrOfTweetObjs.forEach(tweetObj => {
+    $('#tweets-container').prepend(createTweetElement(tweetObj));
+  })
+};
+
+// clear tweets clears all the stored DOM tweets on the html page
+const clearTweets = function(value) {
+  $('.tweet').remove();
+  return value;
+}
+
+// clear the textarea text
+
+const clearTextArea = function(value) {
+  $('textarea').val('');
+  $('.counter').val(140);
+  return value;
+}
+
+// loadtweets makes a GET request to receieve an array of tweets as JSON.
+const loadtweets = function() {
+  $.ajax({method: 'get', url: '/tweets/'})
+  .then(renderTweets)
+  .catch()
+}
+
+const submitAndRecreateTweets = function () {
+  
+}
